f8ae17a163ca0e37f07622d113b1ddc2bd69e4e6
畑本亮太 <hatamotoryouta@hatamotoryoutanoMacBook-Pro.local>
2021-03-10 10:01:02 +0900

    編集

---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/css/flag-icon.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+  <title>旅BOOK</title>
+</head>
+<body>
+<header>
+ <div class="header">
+  <div class="header-top">
+   <div class="header-title">
+    <h1>旅BOOK</h1>
+    <p>〜みんなの思い出でつくるサイト</p>
+    <div class="site-date">
+      <h3>累計投稿数</h3>
+    </div>
+   <div class="user"> 
+     <div class="user-nav">
+       <li><a href="#">ようこそuserさん<span class="fas fa-angle-down"></span></a>
+        <ul>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>マイページ</a></li>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>編集</a></li>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>ログアウト</a></li>
+        </ul>
+      </li>
+     </div>
+   </div>
+  </div>
+</div>
+  <!-- <div class="nav">
+    <ul class="nav-var">
+        <li><a href="#"></li>
+        <li><a href="#">編集</a></li>
+        <li><a href="#">ログアウト</a></li>
+   </ul>
+  </div> -->
+ </div>
+</header>
+
+</body>
+</html>
+
+
+
+<div class="balloon2-top">
+</div>
